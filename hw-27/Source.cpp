30fe10aa4661ac2f3ab3bd1983f0fec905a77baa
CutoffSquire917 <pushkin080109@gmail.com>
2024-12-18 23:02:42 +0100

    'Pre-final'

--- hw-27/Source.cpp
+++ hw-27/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+class RoomBooking;
+
 class Customer {
 private:
 	string name;
@@ -8,11 +10,9 @@ private:
 	static int countID;
 	RoomBooking* currentBooking;
 public:
-	Customer(const string name) : name(name), customerID(countID), currentBooking(nullptr) { countID++; }
-	Customer(const string name, RoomBooking* currentBooking)
-		: name(name), customerID(countID), currentBooking(currentBooking) {
-		countID++;
-	}
+	Customer() : name(""), customerID(false), currentBooking(nullptr) {}
+	Customer(const string& name)
+		: name(name), customerID(countID++), currentBooking(nullptr) {}
 	void makeBooking(RoomBooking* booking) {
 		currentBooking = booking;
 	}
@@ -22,36 +22,25 @@ public:
 	void printCustomerInfo() const {
 		cout << "Name : " << name << endl;
 		cout << "ID : " << customerID << endl;
+		if (currentBooking != nullptr) {
+			//currentBooking->printBooking();
+			cout << "Fuck off" << endl;
+		}
+		else {
+			cout << "No current booking" << endl;
+		}
 	}
-
+	friend ostream& operator<<(ostream& os, const Customer& other) {
+		os << "Name : " << other.name << endl;
+		os << "ID : " << other.customerID;
+		return os;
+	}
 	~Customer() {
 		delete currentBooking;
 	}
 };
 
 int Customer::countID = 1;
-
-//Клас Customer
-//Поля:
-
-//std::string name — ім’я клієнта.
-//int customerID — унікальний ідентифікатор клієнта.
-//RoomBooking* currentBooking — вказівник на поточне бронювання (може бути nullptr, якщо бронювання немає).
-//Конструктори:
-
-//Конструктор за замовчуванням: Створює клієнта без бронювання.
-//Параметризований конструктор: Приймає ім’я клієнта та його ID.
-//Деструктор:
-
-//Звільняє ресурси, пов’язані з бронюванням (за потреби).
-//Методи:
-
-//void makeBooking(RoomBooking* booking) — пов’язує клієнта з бронюванням.
-//void cancelBooking() — скасовує поточне бронювання, видаляючи зв’язок із клієнтом.
-//void printCustomerInfo() — виводить ім’я клієнта, ID та інформацію про бронювання.
-//Перевантаження операторів:
-
-//Оператор << для виведення інформації про клієнта.
 
 class RoomBooking {
 private:
@@ -63,98 +52,124 @@ public:
 	RoomBooking() : roomNumber(false), daysBooked(nullptr), daysCount(false), customer(nullptr) {}
 	RoomBooking(const int roomNumber, const size_t daysCount, Customer* customer)
 		: roomNumber(roomNumber), daysCount(daysCount), customer(customer) {
+		customer->makeBooking(this);
 		daysBooked = new int[daysCount];
 		for (size_t i = 0; i < daysCount; i++)
 		{
 			daysBooked[i] = i + 1;
 		}
 	}
-
-
-	//Конструктор копіювання: Створює глибоку копію об’єкта, включаючи зв’язок із клієнтом.
-
-
-
-	//Конструктор переміщення: Передає ресурси (масив днів і зв’язок із клієнтом).
-
-	//void addDay(int day) — додає день у список заброньованих.
-	void addDays(const int daysCount) {
-		this->daysCount += daysCount;
-		delete daysBooked;
+	RoomBooking(const RoomBooking &other) {
+		roomNumber = other.roomNumber;
+		daysCount = other.daysCount;
+		customer = other.customer;
+		delete[] daysBooked;
 		daysBooked = new int[daysCount];
-		for (size_t i = 0; i < this->daysCount; i++)
+		for (size_t i = 0; i < daysCount; i++)
 		{
 			daysBooked[i] = i + 1;
 		}
 	}
-	//void removeDay(int day) — видаляє день із списку заброньованих.
-	void removeDays(const int daysCount) {
-		if (this->daysCount - daysCount <= 0) {
-			daysBooked = nullptr;
-			this->daysCount = false;
-			return;
+	RoomBooking(RoomBooking& other) {
+		roomNumber = other.roomNumber;
+		daysCount = other.daysCount;
+		customer = other.customer;
+		other.customer = nullptr;
+		delete[] other.daysBooked;
+		delete[] daysBooked;
+		daysBooked = new int[daysCount];
+		for (size_t i = 0; i < daysCount; i++)
+		{
+			daysBooked[i] = i + 1;
 		}
-		this->daysCount -= daysCount;
-		delete daysBooked;
+	}
+	void addDays(const size_t daysCount) {
+		this->daysCount += daysCount;
+		delete[] daysBooked;
 		daysBooked = new int[this->daysCount];
 		for (size_t i = 0; i < this->daysCount; i++)
 		{
 			daysBooked[i] = i + 1;
 		}
 	}
-	//void printBooking() — виводить інформацію про бронювання.
+	void removeDays(const size_t daysCount) {
+		if (int(this->daysCount) - int(daysCount) <= 0) {
+			delete[] daysBooked;
+			customer->cancelBooking();
+			customer = nullptr;
+			return;
+		}
+		this->daysCount -= daysCount;
+		delete[] daysBooked;
+		daysBooked = new int[this->daysCount];
+		for (size_t i = 0; i < this->daysCount; i++)
+		{
+			daysBooked[i] = i + 1;
+		}
+	}
 	void printBooking() const {
 		cout << "roomNumber : " << roomNumber << endl;
 		cout << "daysCount : " << daysCount << endl;
 	}
-
-	//Оператор = для копіювання.
-	//Оператор = для переміщення.
-	//Оператор + для об’єднання бронювань (з перевіркою, що клієнт той самий).
-	//Оператор << для виведення інформації про об’єкт.
-
+	RoomBooking operator=(const RoomBooking& other) {
+		roomNumber = other.roomNumber;
+		daysCount = other.daysCount;
+		customer = other.customer;
+		delete[] daysBooked;
+		daysBooked = new int[daysCount];
+		for (size_t i = 0; i < daysCount; i++)
+		{
+			daysBooked[i] = i + 1;
+		}
+	}
+	RoomBooking operator=(RoomBooking& other) {
+		roomNumber = other.roomNumber;
+		daysCount = other.daysCount;
+		customer = other.customer;
+		other.customer = nullptr;
+		delete[] other.daysBooked;
+		delete[] daysBooked;
+		daysBooked = new int[daysCount];
+		for (size_t i = 0; i < daysCount; i++)
+		{
+			daysBooked[i] = i + 1;
+		}
+	}
+	RoomBooking operator+(const RoomBooking& other) {
+		if (other.customer != customer || other.roomNumber != roomNumber) {
+			throw std::out_of_range("Customer or room is not same");
+		}
+		return RoomBooking(roomNumber, daysCount + other.daysCount, customer);
+	}
+	friend ostream& operator<<(ostream& os, const RoomBooking& other) {
+		if (!other.customer) {
+			return os;
+		}
+		os << "roomNumber : " << other.roomNumber << endl;
+		os << "daysCount : " << other.daysCount;
+		return os;
+	}
 	~RoomBooking() {
 		if (daysCount) {
-			delete daysBooked;
+			delete[] daysBooked;
 		}
-		if (customer != nullptr) {
+		if (customer) {
+			customer->cancelBooking();
 			delete customer;
 		}
-
 	}
 };
 
-//Клас RoomBooking
-//Поля:
-
-//int roomNumber — номер кімнати.
-//int* daysBooked — вказівник на масив днів, на які заброньовано номер.
-//(Наприклад, масив {1, 2, 3} означає, що номер заброньовано на 1-й, 2-й і 3-й дні місяця. Якщо масив порожній, кімната вільна.)
-//size_t daysCount — кількість днів, на які заброньовано номер.
-//Customer* customer — вказівник на клієнта, який забронював кімнату.
-//Конструктори:
-
-//Конструктор за замовчуванням: Створює порожнє бронювання.
-//Параметризований конструктор: Приймає номер кімнати, масив днів, кількість днів та вказівник на клієнта.
-//Конструктор копіювання: Створює глибоку копію об’єкта, включаючи зв’язок із клієнтом.
-//Конструктор переміщення: Передає ресурси (масив днів і зв’язок із клієнтом).
-//Деструктор:
-
-//Звільняє пам’ять, виділену для масиву днів.
-//Методи:
-
-//void addDay(int day) — додає день у список заброньованих.
-//void removeDay(int day) — видаляє день із списку заброньованих.
-//void printBooking() — виводить інформацію про бронювання.
-//Перевантаження операторів:
-
-//Оператор = для копіювання.
-//Оператор = для переміщення.
-//Оператор + для об’єднання бронювань (з перевіркою, що клієнт той самий).
-//Оператор << для виведення інформації про об’єкт.
-
 int main()
 {
-
-
+	Customer* Ivan = new Customer("Ivan");
+	cout << *Ivan << endl;
+	RoomBooking* nomer16 = new RoomBooking(16, 7, Ivan);
+	RoomBooking* nomer17 = new RoomBooking(16, 7, Ivan);
+	RoomBooking nomer18 = *nomer16 + *nomer17;
+	//RoomBooking* nomer17 = new RoomBooking(16, 3, Ivan);
+	//nomer16 = nomer16 + nomer17;
+	cout << nomer18 << endl;
+	cout << endl;
+	Ivan->printCustomerInfo();
 }
